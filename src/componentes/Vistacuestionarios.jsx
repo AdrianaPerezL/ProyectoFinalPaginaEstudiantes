f112d4dad9794e8e13e5991270d6b234ca7105ff
FatimaG04 <oseguedarosales@gmail.com>
2023-03-30 11:04:33 -0600

    avance
    Vista cuestionarios

---
+++ src/componentes/Vistacuestionarios.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+ export const Vistacuestionarios = ()=> {
+    <>
+      
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img class="rounded mx-auto d-block" src="assets/images/Recurso 1.png" width="80px"></img>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#hero">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contactanos</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+     
+
+
+
+     
+    </>
+}
