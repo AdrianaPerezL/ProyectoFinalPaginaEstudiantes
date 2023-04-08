fd296e057f71026e93521c3cd0d4d9506a1586ab
Vpineda <123499235+Vpinedabootcamp@users.noreply.github.com>
2023-04-08 01:42:41 -0600

    avance progreso
    avance progreso

--- src/componentes/vistas/Progreso.jsx
+++ src/componentes/vistas/Progreso.jsx
@@ -26,7 +26,28 @@ export const Progreso = () => {
           <h1>Tu Progreso</h1>
     </div>
 
-      <section id='insignias'>
+    <section id='progreso'> 
+<section id="advertisers" class="advertisers-service-sec pt-5 pb-5">
+  <div class="container">
+    <div class="row">
+    </div>
+    <div class="row mt-5 mt-md-4 row-cols-1 row-cols-sm-1 row-cols-md-3">
+      <div class="col">
+        <div class="service-card">
+          <div class="icon-wrapper">
+            <i class="fa-solid fa-chart-line"></i>
+          </div>
+          <h3>Tracking Lead</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Quisquam consequatur necessitatibus eaque.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <section id='insignias'>
         <div class="accordion accordion-flush" id="accordionFlushExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
@@ -45,7 +66,7 @@ export const Progreso = () => {
       </button>
     </h2>
     <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body.</div>
     </div>
   </div>
   <div class="accordion-item">
@@ -55,7 +76,7 @@ export const Progreso = () => {
       </button>
     </h2>
     <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body.</div>
     </div>
   </div>
 
@@ -82,8 +103,10 @@ export const Progreso = () => {
   </div>
 </div>
 </section>
-
+</section>
+</section>
     </div>
+
 
      
         </>
